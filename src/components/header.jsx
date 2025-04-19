9ac9234c80ec0e885b0a79a9843c57c66ddbf751
Dilshan <it21180934@my.sliit.lk>
2025-04-19 20:50:17 +0530

    init ✏️

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import UserData from "./userData";
+
+export default function Header(){
+    console.log("Header component loading...")
+    return(
+        <div className="bg-[#FFFF00] ">
+            <h1 className="font-bold text-[300px] text-blue-700">Crystal Beauty Clear</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi sit, corporis ratione vel ullam provident expedita temporibus tempora repellendus magnam dolores commodi dicta enim eius consequuntur iste nemo voluptas assumenda.</p>
+            <UserData></UserData>
+        </div>
+    )
+}
