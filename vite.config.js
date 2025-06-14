95d55bc06ae1f6ad794846292ab5fa4fa66ab189
DilshanGAM <it21180934@my.sliit.lk>
2025-06-14 20:59:40 +0530

    Day 20 ✏️

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 })
+//37337765557-5qck0erqtvg70n7fuldt6t82ovtvgrfo.apps.googleusercontent.com
+//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
